da1e1db82ad46697ee76ad76f4a27d9d3491eac5
Alessio Spina <alessiospina93@hotmail.it>
2022-09-27 22:58:19 +0200

    [added] dashboard template

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import "reflect-metadata";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    app.useStaticAssets(join(__dirname, '..', '/client/public'))
+    app.setBaseViewsDir(join(__dirname, '..', '/client/views'))
+    app.setViewEngine('hbs')
+    
     await app.listen(3000);
 }
 bootstrap();
